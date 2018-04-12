20c4724c9eb952b04001dd83d3a03d7611a1d578
Christoph <mch@CH3.local>
2018-04-12 16:52:11 +0700

    Updated version

--- site/templates/blog_tools.php
+++ site/templates/blog_tools.php
@@ -1,5 +1,71 @@
 <?php
 
+	function renderBlogEntry($blog, $homepage, $preview=false) {
+
+		$datetime = wire('datetime');
+
+		$blog_html = '';
+
+	    $image_html = '';
+	    $external_html = '';
+	    
+	    $blog_image = $blog->images->first();
+	    if ($blog_image) {
+	  		$thumb = $blog_image->size(600, 150, 'center');
+	  		$image_html .= "<img src='{$thumb->url}' alt='{$thumb->description}' style='width:100%'>";
+	    }
+	    if ($blog->external_url != '') {
+	      $external_html = "<div class='w3-responsive'><iframe class='w3-mobile' src='{$blog->external_url}' width='640' height='360' frameborder='0' webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div>";
+	    }
+	    $blog_description = '';
+	    if ($blog->description) $blog_description = $blog->description.', ';
+	    $tags_text = '';
+	    foreach($blog->tags as $tag) {
+	      $tags_text .= "<span class='w3-tag'>{$tag->title}</span>";
+	    }
+
+	    $blog_link = '';
+	    if ($preview)
+	    $blog_link = "
+	            <p><a href='$blog->url' class='w3-button w3-padding-large w3-theme-l5 w3-border'><b>READ MORE »</b></a></p>
+           ";
+
+
+	    $blog_html .= " 
+	    <div class='w3-card-4 w3-margin w3-theme-l5'>
+	      {$image_html}
+	      {$external_html}
+	      <div class='w3-container'>
+	        <h3><b>{$blog->title}</b></h3>
+	        <h5>{$blog_description}<span class='w3-opacity'>{$datetime->date('F d, Y', $blog->published)} ({$updated_text} {$datetime->relativeTimeStr($blog->modified)})</span></h5>
+	      </div>
+
+	      <div class='w3-container'>
+	        <div style='max-height:120pt;overflow:scroll'>{$blog->body}</div>
+	        <div class='w3-row'>
+	          <div class='w3-col m8 s12'>
+	          	{$blog_link}
+	            <p><a href='$homepage->url' class='w3-button w3-padding-large w3-white w3-border'><b>RETURN »</b></a></p>
+	          </div>
+	          <div class='w3-col m4 w3-hide-small'>
+	            <p><span class='w3-padding-large w3-right'><b>Comments  </b> <span class='w3-tag'>0</span></span></p>
+	          </div>
+	        </div>
+	        <div class='w3-margin-bottom'>{$tags_text}</div>
+	      </div>
+	    </div>
+		  ";
+
+		if (!$preview) {
+			$blog_html .= "
+	            <p><a href='$homepage->url' class='w3-button w3-padding-large w3-white w3-border'><b>RETURN »</b></a></p>
+	         ";
+		}
+
+
+		return $blog_html;
+	}
+
 	function renderBlog() {
 
 		$user = wire('user');
@@ -26,48 +92,10 @@
 
 		$blog_entries = $pages->find($selector);
 		foreach ($blog_entries as $blog) {
-		  	$image_html = '';
-		    $external_html = '';
-		    
-		  	$blog_image = $blog->images->first();
-		  	if ($blog_image) {
-		  		$thumb = $blog_image->size(600, 150, 'center');
-		  		$image_html .= "<img src='{$thumb->url}' alt='{$thumb->description}' style='width:100%'>";
-		  	}
-		    if ($blog->external_url != '') {
-		      $external_html = "<div class='w3-responsive'><iframe class='w3-mobile' src='{$blog->external_url}' width='640' height='360' frameborder='0' webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div>";
-		    }
-		  	$blog_description = '';
-		  	if ($blog->description) $blog_description = $blog->description.', ';
-		    $tags_text = '';
-		    foreach($blog->tags as $tag) {
-		      $tags_text .= "<span class='w3-tag'>{$tag->title}</span>";
-		    }
-
-		  	$blog_html .= "	
-		    <div class='w3-card-4 w3-margin w3-theme-l5'>
-		    	{$image_html}
-		      {$external_html}
-		      <div class='w3-container'>
-		        <h3><b>{$blog->title}</b></h3>
-		        <h5>{$blog_description}<span class='w3-opacity'>{$datetime->date('F d, Y', $blog->published)} ({$updated_text} {$datetime->relativeTimeStr($blog->modified)})</span></h5>
-		      </div>
-
-		      <div class='w3-container'>
-		      	<div style='max-height:120pt;overflow:scroll'>{$blog->body}</div>
-		        <div class='w3-row'>
-		          <div class='w3-col m8 s12'>
-		            <p><a href='$blog->url' class='w3-button w3-padding-large w3-theme-l5 w3-border'><b>READ MORE »</b></a></p>
-		          </div>
-		          <div class='w3-col m4 w3-hide-small'>
-		            <p><span class='w3-padding-large w3-right'><b>Comments  </b> <span class='w3-tag'>0</span></span></p>
-		          </div>
-		        </div>
-		        <div class='w3-margin-bottom'>{$tags_text}</div>
-		      </div>
-		    </div>
-		    <hr>
-		    ";
+			$blog_html .= renderBlogEntry($blog,null,true);
+			$blog_html .= "
+			    <hr>
+			   ";
 		}
 
 		$blog_html .= "
@@ -79,52 +107,3 @@
 
 	}
 
-	function renderBlogEntry($blog, $homepage) {
-
-		$datetime = wire('datetime');
-
-		$blog_html = '';
-
-	    $image_html = '';
-	    $external_html = '';
-	    
-	    $blog_image = $blog->images->first();
-	    if ($blog_image) {
-	      $image_html .= "<img src='{$blog_image->url}' alt='{$blog_image->description}' style='width:100%'>";
-	    }
-	    if ($blog->external_url != '') {
-	      $external_html = "<div class='w3-responsive'><iframe class='w3-mobile' src='{$blog->external_url}' width='640' height='360' frameborder='0' webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div>";
-	    }
-	    $blog_description = '';
-	    if ($blog->description) $blog_description = $blog->description.', ';
-	    $tags_text = '';
-	    foreach($blog->tags as $tag) {
-	      $tags_text .= "<span class='w3-tag'>{$tag->title}</span>";
-	    }
-
-	    $blog_html .= " 
-	    <div class='w3-card-4 w3-margin w3-theme-l5'>
-	      {$image_html}
-	      {$external_html}
-	      <div class='w3-container'>
-	        <h3><b>{$blog->title}</b></h3>
-	        <h5>{$blog_description}<span class='w3-opacity'>{$datetime->date('F d, Y', $blog->published)} ({$updated_text} {$datetime->relativeTimeStr($blog->modified)})</span></h5>
-	      </div>
-
-	      <div class='w3-container'>
-	        <div style='max-height:120pt;overflow:scroll'>{$blog->body}</div>
-	        <div class='w3-row'>
-	          <div class='w3-col m8 s12'>
-	            <p><a href='$homepage->url' class='w3-button w3-padding-large w3-white w3-border'><b>RETURN »</b></a></p>
-	          </div>
-	          <div class='w3-col m4 w3-hide-small'>
-	            <p><span class='w3-padding-large w3-right'><b>Comments  </b> <span class='w3-tag'>0</span></span></p>
-	          </div>
-	        </div>
-	        <div class='w3-margin-bottom'>{$tags_text}</div>
-	      </div>
-	    </div>
-		  ";
-
-		return $blog_html;
-	}
