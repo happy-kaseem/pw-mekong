d5174d93454b55b5df7fe85b6e167b4d50cbeea4
Christoph <mch@CH3.local>
2018-04-12 17:01:06 +0700

    Translation support

--- site/templates/_main.php
+++ site/templates/_main.php
@@ -40,10 +40,13 @@ $prev_page = $page->prev();
 if (($next_page->id) || ($prev_page->id)) {
   if ($next_page->id) $next_href = $next_page->url; else $next_disable = 'w3-disabled';
   if ($prev_page->id) $prev_href = $prev_page->url; else $prev_disable = 'w3-disabled';
+
+  $prev_text = __("Previous");
+  $next_text = __("Next");
   
   $footer_nextprevious .= "
-    <a href = '$prev_href' class='w3-button w3-theme-d5 {$prev_disable} w3-padding-large w3-margin-bottom'>Previous</a>
-    <a href = '$next_href' class='w3-button w3-theme-d5 {$next_disable} w3-padding-large w3-margin-bottom'>Next »</a>
+    <a href = '$prev_href' class='w3-button w3-theme-d5 {$prev_disable} w3-padding-large w3-margin-bottom'>« {$prev_text}</a>
+    <a href = '$next_href' class='w3-button w3-theme-d5 {$next_disable} w3-padding-large w3-margin-bottom'>{$next_text} »</a>
   ";
 }
 
