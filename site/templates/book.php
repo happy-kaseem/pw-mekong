2fa72bc109dd324e9778960709a1219745e80dff
Christoph <mch@CH3.local>
2018-04-13 10:17:09 +0700

    Added book list

--- site/templates/book.php
+++ site/templates/book.php
@@ -6,8 +6,16 @@
  	$page_body = '';
 
  	if($page->bookpagetypes) {
- 		if ($page->bookpagetypes == $pages->get('/processwire/book-page-types/library/')) {
+ 		$bookpagetype_library = $pages->get('/processwire/book-page-types/library/');
+ 		$bookpagetype_book = $pages->get('/processwire/book-page-types/book/');
+ 		$bookpagetype_chapter = $pages->get('/processwire/book-page-types/chapter/');
+ 		if ($page->bookpagetypes == $bookpagetype_library) {
 			$page_heading = '<h1>Library</h1>';
+			$books = $page->children("template=book, bookpagetypes={$bookpagetype_book->id}");
+			foreach ($books as $book) {
+				$page_heading .= "<h2>{$book->title}</h2>";
+			}
+ 		} else if ($page->bookpagetypes == $bookpagetype_book) {
  		} else if ($page->bookpagetypes == $pages->get('/processwire/book-page-types/chapter/')) {
 			$page_heading = '<h2>Chapter</h2>';
  		}
