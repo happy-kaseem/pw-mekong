7a6ea27b270e435d608b399c5ecd49c4faa7871c
Christoph <mch@CH3.local>
2018-04-13 10:29:40 +0700

    Update

--- site/templates/book.php
+++ site/templates/book.php
@@ -13,13 +13,13 @@
 			$page_heading = '<h1>Library</h1>';
 			$books = $page->children("template=book, bookpagetypes={$bookpagetype_book->id}");
 			foreach ($books as $book) {
-				$page_heading .= "<h2>{$book->title}</h2><a href='{$book->url}'><i class='fa fa-book'></i></a>";
+				$page_heading .= "<h2><a href='{$book->url}'><i class='fa fa-book'></i>&nbsp;{$book->title}</a></h2>";
 			}
  		} else if ($page->bookpagetypes == $bookpagetype_book) {
 			$page_heading = "<h1>{$page->title}</h1>";
 			$chapters = $page->children("template=book, bookpagetypes={$bookpagetype_chapter->id}");
-			foreach ($chapters as $chapters) {
-				$page_heading .= "<h2>{$chapters->title}</h2><a href='{$chapter->url}'><i class='fa fa-book'></i></a>";
+			foreach ($chapters as $chapter) {
+				$page_heading .= "<h2>{$chapter->title}</h2><a href='{$chapter->url}'><i class='fa fa-book'></i></a>";
 			}
  		} else if ($page->bookpagetypes == $pages->get('/processwire/book-page-types/chapter/')) {
 			$page_heading = '<h2>Chapter</h2>';
