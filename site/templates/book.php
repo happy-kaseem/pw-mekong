c52e67e90df456b6866dc018b9587f56f0ce71b7
Christoph <mch@CH3.local>
2018-04-13 18:18:43 +0700

    Breadcrumbs

--- site/templates/book.php
+++ site/templates/book.php
@@ -22,26 +22,26 @@
  		$bookpagetype_book = $pages->get('/processwire/book-page-types/book/');
  		$bookpagetype_chapter = $pages->get('/processwire/book-page-types/chapter/');
  		if ($page->bookpagetypes == $bookpagetype_library) {
-			$page_heading = "<h1>Library</h1>";
+			$page_heading .= "<h1>Library</h1>";
 			$books = $page->children("template=book, bookpagetypes={$bookpagetype_book->id}");
 			foreach ($books as $book) {
 				$page_heading .= "<h2><a href='{$book->url}'><i class='fa fa-book'></i>&nbsp;{$book->title}</a></h2>";
 			}
  		} else if ($page->bookpagetypes == $bookpagetype_book) {
-			$page_heading = "<h1>{$page->title}</h1>";
+			$page_heading .= "<h1>{$page->title}</h1>";
 			$chapters = $page->children("template=book, bookpagetypes={$bookpagetype_chapter->id}");
 			foreach ($chapters as $chapter) {
 				$page_heading .= "<h2><a href='{$chapter->url}'><i class='fa fa-file-text'></i>&nbsp;{$chapter->title}</a></h2>";
 			}
  		} else if ($page->bookpagetypes == $pages->get('/processwire/book-page-types/chapter/')) {
-			$page_heading = "<h2>{$page->title}</h2>";
+			$page_heading .= "<h2>{$page->title}</h2>";
 			$chapters = $page->children("template=book, bookpagetypes={$bookpagetype_chapter->id}");
 			foreach ($chapters as $chapter) {
 				$page_heading .= "<h3><a href='{$chapter->url}'><i class='fa fa-file-text'></i>&nbsp;{$chapter->title}</a></h3>";
 			}
  		}
 	} else {
-		$page_heading = "<h2>{$page->title}</h2>";
+		$page_heading .= "<h2>{$page->title}</h2>";
 	}
 	$content = "
 	<div class='w3-container'>
