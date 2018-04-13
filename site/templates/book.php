8585a16823a7315c75708c6bc81f5b7ee8644680
Christoph <mch@CH3.local>
2018-04-13 10:27:17 +0700

    Added chapters

--- site/templates/book.php
+++ site/templates/book.php
@@ -13,9 +13,14 @@
 			$page_heading = '<h1>Library</h1>';
 			$books = $page->children("template=book, bookpagetypes={$bookpagetype_book->id}");
 			foreach ($books as $book) {
-				$page_heading .= "<h2>{$book->title}</h2>";
+				$page_heading .= "<h2>{$book->title}</h2><a href='{$book->url}'><i class='fa fa-book'></i></a>";
 			}
  		} else if ($page->bookpagetypes == $bookpagetype_book) {
+			$page_heading = "<h1>{$page->title}</h1>";
+			$chapters = $page->children("template=book, bookpagetypes={$bookpagetype_chapter->id}");
+			foreach ($chapters as $chapters) {
+				$page_heading .= "<h2>{$chapters->title}</h2><a href='{$chapter->url}'><i class='fa fa-book'></i></a>";
+			}
  		} else if ($page->bookpagetypes == $pages->get('/processwire/book-page-types/chapter/')) {
 			$page_heading = '<h2>Chapter</h2>';
  		}
