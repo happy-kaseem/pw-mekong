5f51bc74926b60620bfaf7aec3771fcf32dde329
Christoph <mch@CH3.local>
2018-04-13 18:23:31 +0700

    Breadcrumbs

--- site/templates/book.php
+++ site/templates/book.php
@@ -9,11 +9,9 @@
 
  	if (count($parents)>0) {
  		$page_heading .= "<div class='w3-container'>";
- 		$breadcrumbs = array();
  		foreach ($parents as $parent) {
-	 		$breadcrumbs[] = "<a class='w3-button' href='{$parent->url}'>{$parent->title}</a>";
+	 		$page_heading .= "<b><i class='fa fa-angle-double-right'></i>&nbsp;<a class='w3-button' href='{$parent->url}'>{$parent->title}</a></b>";
  		}
- 		$page_heading .= implode(' » ', $breadcrumbs);
  		$page_heading .= "</div>";
  	}
 
