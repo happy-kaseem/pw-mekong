a99256cc52fe32130264c28806fce278e79882ce
Christoph <mch@CH3.local>
2018-04-13 10:07:18 +0700

    Updated book page

--- site/templates/book.php
+++ site/templates/book.php
@@ -2,10 +2,21 @@
 
 	include_once('./book_tools.php');
 
+	$page_heading = '';
  	$page_body = '';
+
+ 	if($page->bookpagetypes) {
+ 		if ($page->bookpagetypes == $pages->get('/processwire/book-page-types/library/') {
+			$page_heading = '<h1>Library</h1>';
+ 		} else if ($page->bookpagetypes == $pages->get('/processwire/book-page-types/chapter/') {
+			$page_heading = '<h2>Chapter</h2>';
+ 		}
+	}
 	$content = "
 	<div class='w3-container'>
+		{$page_heading}
 		{$page->body}
+		}
 	</div>
 	";
 	
