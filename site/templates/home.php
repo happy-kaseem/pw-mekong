3e7d7181e24637563aec75848cb3af386832ad89
Christoph <mch@CH3.local>
2018-04-14 16:20:20 +0700

    Update for guests

--- site/templates/home.php
+++ site/templates/home.php
@@ -14,31 +14,33 @@ if ($image) {
   ";
 }
 
+$content .= $page->body;
+
 if ($user->isLoggedin()) {
 
-$updated_text = __('Updated');
+  $updated_text = __('Updated');
 
-$content .= "
-<!-- Grid -->
-<div class='w3-row'>
+  $content .= "
+  <!-- Grid -->
+  <div class='w3-row'>
 
-<div class='w3-col l3 s12'>
-  <!-- About Card -->
-  <div class='w3-card w3-margin w3-margin-top'>
-  <!-- img src='/w3images/avatar_g.jpg' style='width:100%' -->
-    <div class='w3-container w3-theme-l5'>
-      {$page->about}
-    </div>
-  </div><hr>
-</div>
-";
+  <div class='w3-col l3 s12'>
+    <!-- About Card -->
+    <div class='w3-card w3-margin w3-margin-top'>
+    <!-- img src='/w3images/avatar_g.jpg' style='width:100%' -->
+      <div class='w3-container w3-theme-l5'>
+        {$page->about}
+      </div>
+    </div><hr>
+  </div>
+  ";
 
-$blog_html = renderBlog();
-$content .= $blog_html; 
+  $blog_html = renderBlog();
+  $content .= $blog_html; 
 
-$content .= "
-<!-- END Grid -->
-</div>
-";
+  $content .= "
+  <!-- END Grid -->
+  </div>
+  ";
 
 }; // END if ($user->isLoggedin())
