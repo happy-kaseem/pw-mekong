70bcf7e04bdea4b6be9631b4b3668d334cf57be5
Christoph <mch@192.168.0.35>
2018-04-18 19:08:04 +0700

    Space separator between tags

--- site/templates/blog_tools.php
+++ site/templates/blog_tools.php
@@ -139,7 +139,7 @@
 
 		foreach ($taglist as $key => $tag) {
 			$filtered_url = $page->url."?tagfilter={$key}";
-			$html .= "<span class='w3-tag'><a href='{$filtered_url}'>{$tag}</a></span><br />";
+			$html .= "<span class='w3-tag'><a href='{$filtered_url}'>{$tag}</a></span>&nbsp;";
 		}
 
 		return $html;
