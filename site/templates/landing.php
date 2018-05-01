22faa83bb8519be1d652df17136c06efdebbd697
Christoph <mch@192.168.0.35>
2018-05-01 10:25:11 +0700

    Token test

--- site/templates/landing.php
+++ site/templates/landing.php
@@ -15,8 +15,14 @@ if ($user->isLoggedin()) {
 	$selector = "name={$urlsegment}";
 	$log->save('messages', $selector);
 	$token = $pages->get('/processwire/landing-tokens/')->find($selector);
-	if ((count($token)==1) and ($token->token_expiration>=now())) {
-		$comment = 'valid token!';
+	if (count($token)==1) {
+		if ($token->token_expiration>=now()) {
+			$comment = 'valid token!';
+		} else {
+			$comment = 'expired token!';
+		}
+	} else {
+		$comment = 'no token!';
 	}
 	$content .= "
 	<!-- Main content -->
