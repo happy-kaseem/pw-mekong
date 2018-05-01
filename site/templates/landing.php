a7c5be67dbf4a612f70de193952eace123a4fe60
Christoph <mch@192.168.0.35>
2018-05-01 10:19:01 +0700

    Token

--- site/templates/landing.php
+++ site/templates/landing.php
@@ -10,12 +10,18 @@ if ($user->isLoggedin()) {
 	";
 } else { // ELSE if ($user->isLoggedin())
 
+	$comment = 'no comment';
 	$urlsegment = $input->urlSegment(1);
+	$token = $pages->get('/processwire/landing-tokens/')->find($urlsegment);
+	if ((count($token)==1) and ($token->token_expiration>=now())) {
+		$comment = 'valid token!';
+	}
 	$content .= "
 	<!-- Main content -->
 	<div class='w3-container'>
 		<p>Welcome back</p>
 		<p>{$urlsegment}</p>
+		<p>{$comment}</p>
 	 </div>
   	";
 }; // END if ($user->isLoggedin())
