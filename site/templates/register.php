ef970d5ae307bfb0a7a85e18433442b799e9753d
Christoph <mch@192.168.0.35>
2018-05-03 09:31:06 +0700

    Add new registration template

---
+++ site/templates/register.php
@@ -0,0 +1,14 @@
+<?php namespace ProcessWire;
+
+if ($user->isLoggedin()) {
+
+	$content .= "
+	<!-- Main content -->
+	<div class='w3-container'>
+
+	</div>
+	";
+} else { // ELSE if ($user->isLoggedin())
+
+
+}; // END if ($user->isLoggedin())
