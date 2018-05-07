45e6274f32050316c244b86a8e53661d9710e11d
Christoph <mch@CH3.local>
2018-05-07 14:36:32 +0700

    Layout, add image

--- site/templates/presentation.php
+++ site/templates/presentation.php
@@ -7,8 +7,15 @@ if ($user->isLoggedin()) {
 	$main = null;
 	$first = $subpages->first();
 	if ($first) {
+		$imagehtml = null;
+		$image = $first->images->first();
+		if ($image) {
+			$tumb = $image->size(60,180);
+			$imagehtml = "<img src='{$tumb->url}' class='w3-image'>";
+		}
 		$main .= "
-		<div class='{$cardstyle} w3-animate-left'>
+		<div class='w3-container w3-animate-left'>
+			{$imagehtml}
 			<div class='w3-container w3-large w3-theme' style='height:50px'>
 				{$first->title}
 			</div>
@@ -24,7 +31,7 @@ if ($user->isLoggedin()) {
 		// height 172px = 50+90+2*16 (margins and padding)
 		$menu .= "
 		<div class='w3-container w3-col l2 m3 s6' style='height: 172px;'>
-			<div class='w3-theme-l5'>
+			<div class='w3-theme-l4'>
 				<div class='w3-container w3-theme' style='height:50px'>
 					{$sp->title}
 				</div>
