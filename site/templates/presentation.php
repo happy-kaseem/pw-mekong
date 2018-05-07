a8949fe96f6d73dc8cefe26d3b64140c3b0162b3
Christoph <mch@CH3.local>
2018-05-07 14:30:08 +0700

    Layout

--- site/templates/presentation.php
+++ site/templates/presentation.php
@@ -23,8 +23,8 @@ if ($user->isLoggedin()) {
 	foreach ($subpages as $sp) {
 		// height 172px = 50+90+2*16 (margins and padding)
 		$menu .= "
-		<div class='w3-col l2 m3 s6' style='height: 172px;'>
-			<div class='w3-style-l5'>
+		<div class='w3-container w3-col l2 m3 s6' style='height: 172px;'>
+			<div class='w3-theme-l5'>
 				<div class='w3-container w3-theme' style='height:50px'>
 					{$sp->title}
 				</div>
