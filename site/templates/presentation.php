e981b98dba2f4ed1698437ea9b01d0facdadf52e
Christoph <mch@CH3.local>
2018-05-07 09:28:36 +0700

    More layout

--- site/templates/presentation.php
+++ site/templates/presentation.php
@@ -8,12 +8,12 @@ if ($user->isLoggedin()) {
 	$menu = "<div class='w3-row-padding'>";
 	foreach ($subpages as $sp) {
 		$menu .= "
-		<div class='w3-col l2 m3 s6' style='height:120px'>
+		<div class='w3-col l2 m3 s6' style='height:140px'>
 			<div class='w3-card'>
-				<div class='w3-container w3-theme' style='height:40px'>
+				<div class='w3-container w3-theme' style='height:50px'>
 					{$sp->title}
 				</div>
-				<div class='w3-container' style='height:80px;overflow:auto'>
+				<div class='w3-container w3-small' style='height:90px;overflow:auto'>
 					{$sp->about}
 				</div>
 			</div>
