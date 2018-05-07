9e804132265eddb6e84990c56e4e76300220b366
Christoph <mch@CH3.local>
2018-05-07 14:00:33 +0700

    Update layout

--- site/templates/presentation.php
+++ site/templates/presentation.php
@@ -21,11 +21,11 @@ if ($user->isLoggedin()) {
 		";	
 	}
 
-	$menu = "<div class='w3-row w3-padding'>";
+	$menu = "<div class='w3-row'>";
 	foreach ($subpages as $sp) {
 		// height 172px = 50+90+2*16 (margins and padding)
 		$menu .= "
-		<div class='w3-col l2 m3 s6' style='height: 172px;'>
+		<div class='w3-col l2 m3 s6 w3-padding' style='height: 172px;'>
 			<div class='{$cardstyle} w3-margin'>
 				<div class='w3-container w3-theme' style='height:50px'>
 					{$sp->title}
