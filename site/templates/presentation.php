573a46e58ae92b8f02bdd093d4558b4a35d1456c
Christoph <mch@CH3.local>
2018-05-07 14:08:39 +0700

    Layout

--- site/templates/presentation.php
+++ site/templates/presentation.php
@@ -26,7 +26,7 @@ if ($user->isLoggedin()) {
 		// height 172px = 50+90+2*16 (margins and padding)
 		$menu .= "
 		<div class='w3-col l2 m3 s6' style='height: 172px;'>
-			<div class='{$cardstyle} w3-margin'>
+			<div class='{$cardstyle}'>
 				<div class='w3-container w3-theme' style='height:50px'>
 					{$sp->title}
 				</div>
