85e7d267ee130d87874e5a9b1c1ca5824e47fabc
Christoph <mch@CH3.local>
2018-05-07 09:27:15 +0700

    More layout

--- site/templates/presentation.php
+++ site/templates/presentation.php
@@ -10,10 +10,10 @@ if ($user->isLoggedin()) {
 		$menu .= "
 		<div class='w3-col l2 m3 s6' style='height:120px'>
 			<div class='w3-card'>
-				<div class='w3-container w3-theme' style='height:30px'>
+				<div class='w3-container w3-theme' style='height:40px'>
 					{$sp->title}
 				</div>
-				<div class='w3-container' style='height:90px'>
+				<div class='w3-container' style='height:80px;overflow:auto'>
 					{$sp->about}
 				</div>
 			</div>
